af8d23bc296f22577ed8e27a78320fbc3fc26793
NavyaGargpy <104048802+NavyaGargpy@users.noreply.github.com>
2022-05-26 20:14:17 +0530

    Add files via upload

---
+++ assign04_q01.py
@@ -0,0 +1,15 @@
+a = int(input("enter marks: "))
+#marks between 0-100 are entered
+#now for grades
+if a >= 80:
+    print("A") 
+elif a >= 60:
+    print("B")  
+elif a >= 50:
+    print("C")   
+elif a >= 45:
+    print("D")
+elif a >= 25:
+    print("E")
+else:
+    print("F")        
